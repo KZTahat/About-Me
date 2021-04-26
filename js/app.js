cfbcb4bcd183344d51c5f41524386c98309abd72
KZTahat <kztahat96@gmail.com>
2021-04-26 15:42:52 +0300

    replace switch with if and adding the second qustion

--- js/app.js
+++ js/app.js
@@ -3,19 +3,20 @@
 alert('Hello there, This is About Me guessing game!');
 alert('I\'m going to ask you five questions about myself and you will try to guess');
 
-let acceptence = prompt('So, are you ready?');
+let acceptance = prompt('So, are you ready?').toLowerCase();
+console.log(acceptance);
 
-switch (acceptence.toLowerCase) {
-    case yes:
+if(acceptance == 'yes') {
+        alert('Ok then, let\'s start...')
         let foodType = prompt('What do you think is my favoraite type of food?');
         console.log('Food Type: '+ foodType);
-        
-        break;
-    case no:
-        alert('Ok, nice to meet you anyway :)');
-        break;
-    default:
-        alert("please start again and inter 'yes' or 'no' as an answer!");
-        break;
-}
 
+        let favHobby = prompt('Can you guess my favoraite hobby?');
+        console.log('Favoraite Hobby: '+ favHobby);
+
+
+}else if (acceptance == 'no'){
+    alert('Ok, nice to meet you anyway :)');
+}else {
+    alert("please start again and inter 'yes' or 'no' as an answer!");
+}
